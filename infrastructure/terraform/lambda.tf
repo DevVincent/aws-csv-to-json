15e1f9d46e41b2f8f669691e058fb781240a9966
Vicente Manzano <vicente.manzano@firemind.io>
2022-02-27 19:34:06 +0000

    glup glup

--- infrastructure/terraform/lambda.tf
+++ infrastructure/terraform/lambda.tf
@@ -39,7 +39,7 @@ resource "aws_lambda_function" "lambda_csv_to_json" {
   filename      = "lambda.zip"
   function_name = "${var.SERVICE}-csv-to-json"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "lambda-handler"
+  handler       = "function.lambda-handler"
   runtime       = "python3.9"
 }
 
