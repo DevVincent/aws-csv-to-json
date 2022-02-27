e09e021737f92896008e94712a3e391766e5bccc
Vicente Manzano <vicente.manzano@firemind.io>
2022-02-27 18:21:07 +0000

    confg event

--- infrastructure/terraform/s3.tf
+++ infrastructure/terraform/s3.tf
@@ -8,12 +8,12 @@ resource "aws_s3_bucket" "snoop_raw_data" {
 resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
   bucket = aws_s3_bucket.snoop_raw_data.id
   lambda_function {
-    lambda_function_arn = module.lambda_function.arn
+    lambda_function_arn = module.lambda_function.lambda_function_arn
     events              = ["s3:ObjectCreated:*"]
   }
   depends_on = [
-    module.lambda_function.arn
-      ]
+    module.lambda_function.lambda_function_arn
+  ]
 }
 
 resource "aws_s3_bucket" "snoop_json_data" {
