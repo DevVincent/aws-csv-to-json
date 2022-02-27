7a594ceb77555118097335bc07ba8401d85fafbf
Vicente Manzano <vicente.manzano@firemind.io>
2022-02-27 17:59:23 +0000

    added event

--- infrastructure/terraform/s3.tf
+++ infrastructure/terraform/s3.tf
@@ -1,11 +1,19 @@
-resource "aws_s3_bucket" "snoop-raw-data" {
+resource "aws_s3_bucket" "snoop-raw" {
   bucket_prefix = "${var.SERVICE}-raw-${var.REGION}-snoop-app"
 
   tags          = local.common_tags
 
 }
 
-resource "aws_s3_bucket" "snoop-json-data" {
+resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
+  bucket = aws_s3_bucket.snoop-raw.id
+  lambda_function {
+    lambda_function_arn = module.lambda_function.lambda_function_arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
+
+resource "aws_s3_bucket" "snoop-json" {
   bucket_prefix = "${var.SERVICE}-json-${var.REGION}-snoop-app"
 
   tags          = local.common_tags
