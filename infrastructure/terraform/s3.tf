d8ce9ffcd2f3f40bf6e9663fa0ecc36bea679c2b
Vicente Manzano <vicente.manzano@firemind.io>
2022-02-27 18:04:51 +0000

    rename buckets

--- infrastructure/terraform/s3.tf
+++ infrastructure/terraform/s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "snoop-raw" {
+resource "aws_s3_bucket" "snoop_raw_data" {
   bucket_prefix = "${var.SERVICE}-raw-${var.REGION}-snoop-app"
 
   tags          = local.common_tags
@@ -6,14 +6,14 @@ resource "aws_s3_bucket" "snoop-raw" {
 }
 
 resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
-  bucket = aws_s3_bucket.snoop-raw.id
+  bucket = aws_s3_bucket.snoop_raw_data.id
   lambda_function {
     lambda_function_arn = module.lambda_function.lambda_function_arn
     events              = ["s3:ObjectCreated:*"]
   }
 }
 
-resource "aws_s3_bucket" "snoop-json" {
+resource "aws_s3_bucket" "snoop_json_data" {
   bucket_prefix = "${var.SERVICE}-json-${var.REGION}-snoop-app"
 
   tags          = local.common_tags
